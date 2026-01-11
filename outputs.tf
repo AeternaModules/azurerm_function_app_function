21e5cbe56214aab980eb847c7c72818f53d09ba1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:56:00 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "function_app_functions" {
+  description = "All function_app_function resources"
+  value       = azurerm_function_app_function.function_app_functions
+}
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "function_app_functions" {
+  description = <<EOT
+Map of function_app_functions, attributes below
+Required:
+    - config_json
+    - function_app_id
+    - name
+Optional:
+    - enabled
+    - language
+    - test_data
+    - file (block):
+        - content (required)
+        - name (required)
+EOT
+
+  type = map(object({
+    config_json     = string
+    function_app_id = string
+    name            = string
+    enabled         = optional(bool, true)
+    language        = optional(string)
+    test_data       = optional(string)
+    file = optional(object({
+      content = string
+      name    = string
+    }))
+  }))
+}
+
